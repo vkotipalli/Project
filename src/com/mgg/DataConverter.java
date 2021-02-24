6c42e100dae52c0ab3a07b334fe35b717dcb99af
madisonsmacbookair <madisonsmacbookair@localhost>
2021-02-23 20:10:18 -0600

    Tweaked and added load functions for Persons and Store.

--- src/com/mgg/DataConverter.java
+++ src/com/mgg/DataConverter.java
@@ -2,22 +2,59 @@ package com.mgg;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+//import java.io.PrintWriter;
 import java.util.Scanner;
+
+//import com.thoughtworks.xstream.XStream;
+
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class DataConverter {
+	
+	public static List<Store> loadStoreFile() {
+		File storesInput = new File("data/Stores.csv");
+		List<Store> storesList = new ArrayList<>();
+		Scanner s = null;
+		try {
+			s = new Scanner(storesInput);
+			int numOfStores = Integer.parseInt(s.nextLine());
+			System.out.println(numOfStores);
+			while(s.hasNextLine()) {
+				String line = s.nextLine();
+				String token[] = line.split(",");
+				String storeCode = token[0];
+				String managerCode = token[1];
+				String street = token[2];
+				String city = token[3];
+				String state = token[4];
+				String zip = token[5];
+				String country = token[6];
+				
+				Address storeAddress = new Address(street,city,state,zip,country);
+				Store gameStore = new Store(storeCode,managerCode,storeAddress);
+				
+				gameStore.setStoreCode(storeCode);
+				gameStore.setManagerCode(managerCode);
+				gameStore.setStoreAddress(storeAddress);
+				storesList.add(gameStore);
+				gameStore.toString();
+				System.out.println(gameStore);
+				}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 
-	public static void main(String[] args) {
-//		String id = Item.product.getId();
-		//List to add instances of specific persons
-		List<Person> personsList = new ArrayList<>();
-		//List to add multiple optional email's
-		List<String> emailList = new ArrayList<>();
+		}
+		
+		return storesList;
+	}
+	
+	public static List<Person> loadPersonFile(){
 		File f = new File("data/Persons.csv");
 		Scanner s = null;
-		
+		List<Person> personsList = new ArrayList<>();
+		List<String> emailList = new ArrayList<>();
+
 		try {
 			s = new Scanner(f);
 			int numOfPersons = Integer.parseInt(s.nextLine());
@@ -36,15 +73,14 @@ public class DataConverter {
 				String country = token[8];
 
 				int i = 9;
-				while(i < token.length) {
+				while (i < token.length) {
 					emailList.add(token[i]);
 					i++;
-					
+
 				}
-				
-				
-				Address address = new Address(street,city,state,zip,country);
-				Person p = new Person(personCode,type,lastName,firstName,address,emailList);
+
+				Address address = new Address(street, city, state, zip, country);
+				Person p = new Person(personCode, type, lastName, firstName, address, emailList);
 				p.setPersonCode(personCode);
 				p.setType(type);
 				p.setLastName(lastName);
@@ -58,9 +94,21 @@ public class DataConverter {
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}	
+		}
 		s.close();
+		
+		return personsList;
 	}
-	
 
+	public static void main(String[] args) {
+		
+		List<Person> personsList = loadPersonFile();
+
+		System.out.println("");
+		
+
+		List<Store> storesList = loadStoreFile();
+		
+
+	}
 }
--- src/com/mgg/Store.java
+++ src/com/mgg/Store.java
@@ -1,11 +1,17 @@
 package com.mgg;
 
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Scanner;
+
 public class Store {
 	private String storeCode;
-	private Person managerCode;
+	private String managerCode;
 	private Address storeAddress;
 	
-	public Store(String storeCode, Person managerCode, Address storeAddress) {
+	public Store(String storeCode, String managerCode, Address storeAddress) {
 		super();
 		this.storeCode = storeCode;
 		this.managerCode = managerCode;
@@ -20,11 +26,11 @@ public class Store {
 		this.storeCode = storeCode;
 	}
 
-	public Person getManagerCode() {
+	public String getManagerCode() {
 		return managerCode;
 	}
 
-	public void setManagerCode(Person managerCode) {
+	public void setManagerCode(String managerCode) {
 		this.managerCode = managerCode;
 	}
 
@@ -35,5 +41,49 @@ public class Store {
 	public void setStoreAddress(Address storeAddress) {
 		this.storeAddress = storeAddress;
 	}
+
+	@Override
+	public String toString() {
+		return  storeCode + " " + managerCode + " " + storeAddress;
+	}
+	
+//	public List<Store> loadStoreFile() {
+//		File storesInput = new File("data/Stores.csv");
+//		List<Store> storesList = new ArrayList<>();
+//		Scanner s = null;
+//		try {
+//			s = new Scanner(storesInput);
+//			int numOfStores = Integer.parseInt(s.nextLine());
+//			System.out.println(numOfStores);
+//			while(s.hasNextLine()) {
+//				String line = s.nextLine();
+//				String token[] = line.split(",");
+//				String storeCode = token[0];
+//				String managerCode = token[1];
+//				String street = token[2];
+//				String city = token[3];
+//				String state = token[4];
+//				String zip = token[5];
+//				String country = token[6];
+//				
+//				Address storeAddress = new Address(street,city,state,zip,country);
+//				Store gameStore = new Store(storeCode,managerCode,storeAddress);
+//				
+//				gameStore.setStoreCode(storeCode);
+//				gameStore.setManagerCode(managerCode);
+//				gameStore.setStoreAddress(storeAddress);
+//				storesList.add(gameStore);
+//				gameStore.toString();
+//				System.out.println(gameStore);
+//				}
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//
+//		}
+//		
+//		return storesList;
+//	}
+//	
+//	
 
 }
