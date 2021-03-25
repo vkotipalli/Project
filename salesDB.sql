2888a093aca33b60b16d6578cce67edf6d7b280e
JoanneMadison <69692470+JoanneMadison@users.noreply.github.com>
2021-03-25 17:16:31 -0500

    final phase 3

--- salesDB.sql
+++ salesDB.sql
@@ -1,25 +1,14 @@
 -- Name(s): Madison Maloney and Vasavi Kotipalli 
 -- Date: 03/21/2021
--- Purpose of Program: This is a database representation  
+-- Purpose of Program: This is a database representation of the MGG sales system 
 
-use mmaloney;
-
-## Questions to ask: 
-
-# What are examples of things that can be null and not null?
-
-##FOR ITEMSALE ONLY! Is this the right way to go about inserting data into itemSale? Especially with sales that have multiple items. 
-
-drop table if exists Address;
-drop table if exists Person;
-drop table if exists Email; 
-drop table if exists Store; 
-drop table if exists Item; 
-drop table if exists Service; 
-drop table if exists Subscription;
-drop table if exists Product; 
-drop table if exists Sale; 
 drop table if exists SaleItem;
+drop table if exists Sale; 
+drop table if exists Item; 
+drop table if exists Store;
+drop table if exists Email;
+drop table if exists Person;
+drop table if exists Address;
 
 #This table represents the given attributes of an Address. 
 #This is used with both Person and Store addresses respectfully.
@@ -35,38 +24,38 @@ create table Address (
 #This table represents the given attributes of a Person.
 create table Person (
 	personId int not null primary key auto_increment,
-    personCode varchar(255) not null,
+    personCode varchar(255) not null unique,
     type varchar(255),
     lastName varchar(255), 
     firstName varchar(255), 
-    addressId int,
-    foreign key (addressId) references Address(addressId)
+    addressId int not null,
+    foreign key (addressId) references Address(addressId),
+    constraint uniquePerson unique (personId,personCode)
 )engine=InnoDB,collate=latin1_general_cs;
 
 #This table represents the given attributes of an Email.
 create table Email (
 	emailId int not null primary key auto_increment, 
     email varchar(255), 
-    personCode varchar(255) not null
-    #Does personCode need a foreign key?
-    #foreign key (personCode) references Person(personCode)
-    #join email and person where person codes in both tables are equal
+    personId int not null,
+	foreign key (personId) references Person(personId)
 )engine=InnoDB,collate=latin1_general_cs;
 
 #This table represents the given attributes of a Store.
 create table Store (
 	storeId int not null primary key auto_increment, 
-    storeCode varchar(255), 
-    managerId int, 
-    addressId int, 
+    storeCode varchar(255) not null unique, 
+    managerId int not null, 
+    addressId int not null, 
 	foreign key (managerId) references Person(personId),
-    foreign key (addressId) references Address(addressId)
+    foreign key (addressId) references Address(addressId),
+    constraint uniqueStore unique (storeId,storeCode)
 )engine=InnoDB,collate=latin1_general_cs;
 
 #This table represents the given attributes of an Item no matter the specific type.
 create table Item (
 	itemId int not null primary key auto_increment, 
-    itemCode varchar(255) not null, 
+    itemCode varchar(255) not null unique, 
     itemName varchar(255) not null, 
     itemType varchar(255) not null, 
     cost double,
@@ -74,19 +63,21 @@ create table Item (
     employeeId int, 
     beginDate varchar(255), 
     endDate varchar(255),
-    quantity int
+    quantity int,
+    constraint uniqueItem unique (itemId,itemCode)
 )engine=InnoDB,collate=latin1_general_cs;
 
 #This table represents the given attributes of a Sale.
 create table Sale (
 	saleId int not null primary key auto_increment, 
-    saleCode varchar(255), 
+    saleCode varchar(255)not null unique, 
     customerId int not null, 
     storeId int not null, 
     salespersonId int not null, 
     foreign key (customerId) references Person(personId), 
     foreign key (storeId) references Store(storeId), 
-    foreign Key (salespersonId) references Person(personId)
+    foreign Key (salespersonId) references Person(personId),
+    constraint uniqueSale unique (saleId,saleCode)
 )engine=InnoDB,collate=latin1_general_cs;
 
 #This table is a join table joining the shared attributes of Sale and Item.
@@ -99,7 +90,7 @@ create table SaleItem (
 )engine=InnoDB,collate=latin1_general_cs;
 
 
-#Inserting Person Address Test Data
+#Inserting Person-Address Test Data
 insert into Address(addressId, street, city, state, zip, country) values (1, '3464 Evening Sun Dr.','Las Vegas','NV',89117,'US');
 insert into Address(addressId, street, city, state, zip, country) values (2, '2625 N 169th St','Omaha','NE',68116,'US');
 insert into Address(addressId, street, city, state, zip, country) values (3, '2575 Dole St 326','Honolulu','HI',96822,'US');
@@ -111,25 +102,6 @@ insert into Address(addressId, street, city, state, zip, country) values (8, '21
 insert into Address(addressId, street, city, state, zip, country) values (9, '710 Center Dr.','Flushing','NY',11354,'US');
 insert into Address(addressId, street, city, state, zip, country) values (10, '99 Rainbow Ln','Louisa','KY',41230,'US');
 insert into Address(addressId, street, city, state, zip, country) values (21, '25354 Prado De La Felicidad', 'Calabasas', 'CA', 91302, 'US');
-
-
-#Inserting Email Test Data
-insert into Email(emailId, email, personCode) values (1, 'mmaloney2@unl.edu', 'PGHPA5859I');
-insert into Email(emailId, email, personCode) values (2, 'vkotipalli086@gmail.com', '72OFN7R0RL');
-insert into Email(emailId, email, personCode) values (3, 'vasavi.kotipalli@yahoo.com', '72OFN7R0RL');
-insert into Email(emailId, email, personCode) values (4, NULL, 'R0E2QECOTP');
-insert into Email(emailId, email, personCode) values (5, NULL, '4CQ9USQH10');
-insert into Email(emailId, email, personCode) values (6, NULL, 'EVCDH4PET3');
-insert into Email(emailId, email, personCode) values (7, 'Niheala.P@yahoo.com', '8XUXEI4T0W');
-insert into Email(emailId, email, personCode) values (8, 'nponnatha@gmail.com', '8XUXEI4T0W');
-insert into Email(emailId, email, personCode) values (9, NULL, 'UWUA6YGS9X'); 
-insert into Email(emailId, email, personCode) values (10, 'tboinpally275@gamil.com', 'CZDOL8TJ6I');
-insert into Email(emailId, email, personCode) values (11, 'bbuilder123@yahoo.com', 'E3SBKH0OK6');
-insert into Email(emailId, email, personCode) values (12, 'bob.builder@gmail.com', 'E3SBKH0OK6');
-insert into Email(emailId, email, personCode) values (13, 'bobb@hotmail.com', 'E3SBKH0OK6');
-insert into Email(emailId, email, personCode) values (14, 'tomBrady@gmail.com', 'XC9P4020OQ');
-insert into Email(emailId, email, personCode) values (15, 'crystalstraight123@gmail.com', 'AZDOL1KJ0L');
-
 
 #Inserting Person Test Data
 insert into Person(personId, personCode, type, lastName, firstName, addressId) values (1, 'PGHPA5859I','G','Maloney','Madison', 1);
@@ -144,7 +116,24 @@ insert into Person(personId, personCode, type, lastName, firstName, addressId) v
 insert into Person(personId, personCode, type, lastName, firstName, addressId) values (10, 'XC9P4020OQ','G','Brady','Tom', 10);
 insert into Person(personId, personCode, type, lastName, firstName, addressId) values (11, 'AZDOL1KJ0L', 'C', 'Straight', 'Crystal', 21);
 
-#Inserting Store Address Test Data 
+#Inserting Email Test Data
+insert into Email(emailId, email, personId) values (1, 'mmaloney2@unl.edu', 1);
+insert into Email(emailId, email, personId) values (2, 'vkotipalli086@gmail.com', 2);
+insert into Email(emailId, email, personId) values (3, 'vasavi.kotipalli@yahoo.com', 2);
+insert into Email(emailId, email, personId) values (4, NULL, 3);
+insert into Email(emailId, email, personId) values (5, NULL, 4);
+insert into Email(emailId, email, personId) values (6, NULL, 5);
+insert into Email(emailId, email, personId) values (7, 'Niheala.P@yahoo.com', 6);
+insert into Email(emailId, email, personId) values (8, 'nponnatha@gmail.com', 6);
+insert into Email(emailId, email, personId) values (9, NULL, 7); 
+insert into Email(emailId, email, personId) values (10, 'tboinpally275@gamil.com', 8);
+insert into Email(emailId, email, personId) values (11, 'bbuilder123@yahoo.com', 9);
+insert into Email(emailId, email, personId) values (12, 'bob.builder@gmail.com', 9);
+insert into Email(emailId, email, personId) values (13, 'bobb@hotmail.com', 9);
+insert into Email(emailId, email, personId) values (14, 'tomBrady@gmail.com', 10);
+insert into Email(emailId, email, personId) values (15, 'crystalstraight123@gmail.com', 11);
+
+#Inserting Store-Address Test Data 
 insert into Address(addressId, street, city, state, country, zip) values (11, '973 Spring Avenue','Phila','PA',19103,'US');
 insert into Address(addressId, street, city, state, country, zip) values (12, '2434 Mount Tabor','Winchester','TN',37398,'US');
 insert into Address(addressId, street, city, state, country, zip) values (13, '1956 Ashford Drive','Alexandria','VA',22304,'US');
@@ -156,20 +145,17 @@ insert into Address(addressId, street, city, state, country, zip) values (18, '2
 insert into Address(addressId, street, city, state, country, zip) values (19, '4294 Old House Drive','Summerfield','OH',43788,'US');
 insert into Address(addressId, street, city, state, country, zip) values (20, '4021 Gnatty Creek Road','Westbury','NY',11590,'US');
 
-
 #Inserting Store Test Data
-insert into Store(storeId, storeCode, managerId, addressId) VALUES (1, 'HJWPT13Z3V', 6, 11);
-INSERT INTO Store(storeId, storeCode, managerId, addressId) VALUES (2, 'BSNGQH84ZW', 4, 12);
-INSERT INTO Store(storeId, storeCode, managerId, addressId) VALUES (3, 'Y33N4W1E8G', 4, 13);
-INSERT INTO Store(storeId, storeCode, managerId, addressId) VALUES (4, 'YYW8GS50PL', 6, 14);
-INSERT INTO Store(storeId, storeCode, managerId, addressId) VALUES (5, 'BXOLMSCUQV', 4, 15);
-INSERT INTO Store(storeId, storeCode, managerId, addressId) VALUES (6, '9KOICUR4S1', 7, 16);
-INSERT INTO Store(storeId, storeCode, managerId, addressId) VALUES (7, 'H04SOSFZ5J', 6, 17);
-INSERT INTO Store(storeId, storeCode, managerId, addressId) VALUES (8, 'F9ZAUR57XO', 7, 18);
-INSERT INTO Store(storeId, storeCode, managerId, addressId) VALUES (9, 'ZG1F5MHIOG', 6, 19);
-INSERT INTO Store(storeId, storeCode, managerId, addressId) VALUES (10, '100HYR52FL', 7, 20);
-
-
+insert into Store(storeId, storeCode, managerId, addressId) values (1, 'HJWPT13Z3V', 6, 11);
+insert into Store(storeId, storeCode, managerId, addressId) values (2, 'BSNGQH84ZW', 4, 12);
+insert into Store(storeId, storeCode, managerId, addressId) values (3, 'Y33N4W1E8G', 4, 13);
+insert into Store(storeId, storeCode, managerId, addressId) values (4, 'YYW8GS50PL', 6, 14);
+insert into Store(storeId, storeCode, managerId, addressId) values (5, 'BXOLMSCUQV', 4, 15);
+insert into Store(storeId, storeCode, managerId, addressId) values (6, '9KOICUR4S1', 7, 16);
+insert into Store(storeId, storeCode, managerId, addressId) values (7, 'H04SOSFZ5J', 6, 17);
+insert into Store(storeId, storeCode, managerId, addressId) values (8, 'F9ZAUR57XO', 7, 18);
+insert into Store(storeId, storeCode, managerId, addressId) values (9, 'ZG1F5MHIOG', 6, 19);
+insert into Store(storeId, storeCode, managerId, addressId) values (10, '100HYR52FL', 7, 20);
 
 #Inserting Item Test Data
 insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (1, 'fc63ef', 'Cyberpunk 2077', 'PU', 55.99, NULL, NULL, NULL, NULL, 1);
@@ -179,18 +165,14 @@ insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeI
 insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (5, '90a51d', 'Nintendo eShop Credit', 'PG', NULL, NULL, NULL, NULL, NULL, NULL);
 insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (6, 'e4ed68', 'League of Legends Riot Points', 'PG', NULL, NULL, NULL, NULL, NULL, NULL);
 insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (7, '76c395', 'Stardew Valley', 'PU', 10.99, NULL, NULL, NULL, NULL, 3);
-#insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (7, '76c395', 'Stardew Valley', 'PU', 10.99, NULL, NULL, NULL, NULL, 3);
-## STARDEW VALLEY HAS 2
 insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (8, '8b095e', 'Computer Repair', 'SV', 19.99, 6, 9, NULL, NULL, NULL);
 insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (9, '3c43d0', 'Game Room Design', 'SV', 39.99, 4, 7, NULL, NULL, NULL);
-## NINTENDO SWITCH ONLINE HAS 2
 insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (10, '516454', 'Nintendo Switch Online', 'SB', 19.99, NULL, NULL, '2020-06-15', '2021-02-08', NULL);
-#insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (10, '516454', 'Nintendo Switch Online', 'SB', 19.99, NULL, NULL, '2020-06-15', '2021-02-08', NULL);
 insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (11, '7982fe', 'Xbox Magazine', 'SB', NULL, NULL, NULL, NULL, NULL, NULL);
 insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (12, 'd09a87', 'Netflix', 'SB', 101.99, NULL, NULL, '2016-03-23', '2019-11-02', NULL);
 insert into Item(itemId, itemCode, itemName, itemType, cost, numHours, employeeId, beginDate, endDate, quantity) values (13, '774196', 'Apple Arcade', 'SB', 69.99, NULL, NULL, '2018-10-06', '2020-09-29', NULL);
 
-#update statements for query #13
+#update statements for query #13 (giftcards)
 update Item set cost = 100 where itemId=5;
 update Item set cost = 400 where itemId=6;
 
@@ -201,14 +183,10 @@ insert into Sale(saleId, saleCode, customerId, storeId, salespersonId) values (3
 insert into Sale(saleId, saleCode, customerId, storeId, salespersonId) values (4, 'LLQFRHT4HW', 10, 4, 7);
 insert into Sale(saleId, saleCode, customerId, storeId, salespersonId) values (5, 'NUDVU2C0CB', 3, 7, 9);
 insert into Sale(saleId, saleCode, customerId, storeId, salespersonId) values (6, 'OPX58M9BZ8', 2, 9, 4);
-##added sale test data to have a store have multiple sales #8
 insert into Sale(saleId, saleCode, customerId, storeId, salespersonId) values (7, 'PPX58M9BZ9', 2, 9, 4);
-## Added sale instance for query #12
 insert into Sale(saleId, saleCode, customerId, storeId, salespersonId) values (8, 'PPG58W9GA8', 7, 9, 7);
-#Inserting sale statements for query #13
 insert into Sale(saleId, saleCode, customerId, storeId, salespersonId) values (9, 'LWDVU7JWLF', 7, 9, 7);
 insert into Sale(saleId, saleCode, customerId, storeId, salespersonId) values (10, 'NKSZM2UA65', 6, 9, 6);
-
 
 #Inserting SaleItem Test Data
 insert into SaleItem (saleItemId, saleId, itemId) values (1, 1, 10); 
@@ -221,12 +199,8 @@ insert into SaleItem (saleItemId, saleId, itemId) values (7, 4, 13);
 insert into SaleItem (saleItemId, saleId, itemId) values (8, 5, 8);
 insert into SaleItem (saleItemId, saleId, itemId) values (9, 6, 1);
 insert into SaleItem (saleItemId, saleId, itemId) values (10, 6, 10);
-#added saleitem test data to have a store have multiple sales #8
 insert into SaleItem (saleItemId, saleId, itemId) values (11, 7, 8);
-#added saleitem test data to comply with #11
 insert into SaleItem (saleItemId, saleId, itemId) values (12, 3, 7);
-#added saleitem test data statements for query #13
-delete from SaleItem where saleItemId = 15;
 insert into SaleItem (saleItemId, saleId, itemId) values (13, 9, 5);
 insert into SaleItem (saleItemId, saleId, itemId) values (14, 9, 6);
 insert into SaleItem (saleItemId, saleId, itemId) values (15, 10, 6);
--- salesQueries.sql
+++ salesQueries.sql
@@ -14,25 +14,37 @@
 -- 
 -- 3. A query to get the email addresses of a specific person
 	select e.email, p.lastName, p.firstName from Email e 
-    left join Person p on e.personCode = p.personCode 
+    left join Person p on e.personId= p.personId 
     where p.personCode = 'E3SBKH0OK6';
 -- 
 -- 4. A query to change the email address of a specific email record
 	#New email updated:
 	update Email set email = 'mm300@hawaii.edu' where emailId = 4;
 	#Existing email changed: 
+    update Email set email = 'madisonmaloney2002@gmail.com' where emailId = 1;
+    #To prove boths email's was updated (Mallory before didn't have an email and now she has 'mm300@hawaii.edu'
+	#									 Madison's was changed from 'mmaloney2@unl.edu' to 'madisonmaloney2002@gmail.com'): 
+    select * from Email;
 -- 
 -- 5. A query (or series of queries) to remove a specific person record
-	delete from Person where personId = 11;
 	delete from Email where emailId = 15;
+    delete from Person where personId = 11;
 	delete from Address where addressId = 21;
+    #To prove deletion (Crystal does not appear and was deleted): 
+    select * from Person;
 -- 
 -- 6. A query to get all the items on a specific sales record
-select s.saleCode, s.customerId, s.storeId, s.salesPersonId, i.itemId from Sale s left join SaleItem i on s.saleId = i.saleId;
+	select s.saleId, s.saleCode, s.customerId, s.storeId, s.salesPersonId, si.itemId, i.itemName, i.cost from Sale s 
+	left join SaleItem si on s.saleId = si.saleId
+	left join Item i on si.itemId=i.itemId
+	where s.saleCode = 'AB2Q7D53KT';
 -- 
 -- 7. A query to get all the items purchased by a specific person
-select s.saleCode, s.customerId, i.itemId, p.firstName, p.lastName from Sale s 
-left join SaleItem i on s.saleId = i.saleId left join Person p on p.personId=s.customerId where personId = 1;
+	select s.saleCode, s.customerId, i.itemId, i.itemName, i.cost, p.firstName, p.lastName from Sale s 
+	left join SaleItem si on s.saleId = si.saleId 
+    left join Person p on p.personId=s.customerId
+    left join Item i on i.itemId=si.itemId
+    where personId = 1;
 -- 
 -- 8. A query to find the total number of sales made at each store
 	select s.storeId, st.storeCode, count(s.saleId) as numSalesByStore from Sale s 
@@ -43,7 +55,7 @@ left join SaleItem i on s.saleId = i.saleId left join Person p on p.personId=s.c
 	left join Person p on s.salespersonId=p.personId group by s.salespersonId;
 -- 
 -- 10. A query to find the total charge of all services in each sale (hint: you can take an aggregate of a mathematical expression)
--- Author Note: had service query implemented before handout change on 3/25/21.
+-- Author Note: had service query implemented before handout change on 03/25/21.
 	select si.saleId, si.itemId, i.itemCode, s.saleCode, avg(i.cost * i.numHours) as totalCharge from SaleItem si 
 	left join Item i on si.itemId=i.itemid 
 	left join Sale s on s.saleId=si.saleId
@@ -53,8 +65,8 @@ left join SaleItem i on s.saleId = i.saleId left join Person p on p.personId=s.c
 -- 11. A query to detect invalid data in sales as follows. In a single sale, a particular product should only appear once (since any 
 -- number of units can be consolidated to a single record). Write a query to find any sale that includes multiple instances of the same product.
 	select s.saleId, s.saleCode, si.itemId, i.itemName, i.itemCode from Sale s
-	inner join SaleItem si on s.saleId = si.saleId
-    inner join Item i on si.itemId = i.itemId
+	inner join SaleItem si on s.saleId=si.saleId
+    inner join Item i on si.itemId=i.itemId
     group by s.saleId, i.itemId
     having count(s.saleId) > 1;   
 -- 
